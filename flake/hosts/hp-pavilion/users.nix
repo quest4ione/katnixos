e733c1a3ec4c18277161d8122ebdb69f70ad5d66
quest4ione <quest4ione@gmail.com>
2026-09-18 10:08:25 +0200

    add dialout group to user

--- flake/hosts/hp-pavilion/users.nix
+++ flake/hosts/hp-pavilion/users.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.hpPavilionConfiguration = { pkgs, ... }: {
     users.users.quest = {
       isNormalUser = true;
-      extraGroups = [ "wheel" "docker" ];
+      extraGroups = [ "wheel" "docker" "dialout" ];
     };
   };
 }
